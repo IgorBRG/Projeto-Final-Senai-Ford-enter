f8a143d153200ceb1748bd1b92f853e70d7e21e9
Igor Oliveira <Igor Pc Pessoal>
2025-05-19 00:09:34 -0300

    Finalizando Home e adicionando logica de logout para limpar o local storage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,24 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
-
-
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { ApiService } from '../api.service';
 import { SidebarComponent } from '../sidebar/sidebar.component';
-
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
   standalone: true,
-  imports: [CommonModule, FormsModule, SidebarComponent]
+  imports: [CommonModule, SidebarComponent],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  constructor(private router: Router) {}
+export class HomeComponent implements OnInit {
+  nomeUsuario: string | null = null;
 
-  
- 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    const usuarioAtual = this.authService.currentUserValue;
+    if (usuarioAtual) {
+      this.nomeUsuario = usuarioAtual.usuario;
+    }
+  }
 }
